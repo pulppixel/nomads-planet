78bf15ae32082c1e43c70ecdee346cf68a7d63f4
hwankee <devenvy100@gmail.com>
2023-05-21 01:08:42 +0900

    Never Tracked All Assets

--- Assets/Mameshiba Games/Common/Scripts/Utils/Events.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine.Events;
-
-namespace MameshibaGames.Common.Utils
-{
-    [System.Serializable]
-    public class UnityEventInt : UnityEvent<int>
-    {
-    }
-}
--- Assets/Plugins/GameCreator/Packages/Core/Runtime/Common/Polymorphism/Properties/Types/Set/Base/PropertyTypeSetWeapon.cs
+++
@@ -1,9 +0,0 @@
-using System;
-using GameCreator.Runtime.Characters;
-
-namespace GameCreator.Runtime.Common
-{
-    [Serializable]
-    public abstract class PropertyTypeSetWeapon : TPropertyTypeSet<IWeapon>
-    { }
-}
