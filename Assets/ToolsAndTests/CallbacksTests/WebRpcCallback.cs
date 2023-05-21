77543b4891bf8b63ccdbddf257dc3f9a3cefffa1
pulp-pixel <devenvy100@gmail.com>
2023-05-21 22:43:34 +0900

    초기 세팅 및 Gitignore (#8)

--- Assets/ToolsAndTests/CallbacksTests/WebRpcCallback.cs
+++
@@ -1,9 +0,0 @@
-﻿using ExitGames.Client.Photon;
-using Photon.Realtime;
-
-public class WebRpcCallback : BaseCallbacks, IWebRpcCallback
-{
-    public void OnWebRpcResponse(OperationResponse response)
-    {
-    }
-}
