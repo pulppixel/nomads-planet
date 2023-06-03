86eb9699ff8366134934256dcc017ba68a57a4c9
hwankee <devenvy100@gmail.com>
2023-06-04 03:14:43 +0900

    Level Design... ing...

---
+++ Assets/_1_Scripts/BackendManager.cs
@@ -0,0 +1,34 @@
+using System.Threading.Tasks;
+using UnityEngine;
+
+using BackEnd;
+
+public class BackendManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        var bro = Backend.Initialize(true);
+
+        if (bro.IsSuccess())
+        {
+            CustomFunc.WriteLine("초기화 성공: " + bro);
+        }
+        else
+        {
+            CustomFunc.WriteLine("초기화 실패: " + bro, true);
+        }
+
+        Test();
+    }
+
+    async void Test()
+    {
+        await Task.Run(() =>
+        {
+            BackendLogin.Instance.CustomLogin("user1", "1234");
+            // BackendLogin.Instance.UpdateNickname("바보2");
+            CustomFunc.WriteLine("테스트 종료");
+        });
+    }
+}
---
+++ Assets/_1_Scripts/CustomFunc.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public static class CustomFunc
+{
+    public static void WriteLine(System.Object message, bool isError = false)
+    {
+#if UNITY_EDITOR
+        if (isError)
+        {
+            Debug.LogError(message);
+        }
+        else
+        {
+            Debug.Log(message);
+        }
+#endif
+    }
+}
