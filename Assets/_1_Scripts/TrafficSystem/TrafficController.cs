a08d38e2e4fc3a81ecef357b65024075f2d133db
hwankee <devenvy100@gmail.com>
2023-07-09 23:27:10 +0900

    CONSTANT
    Co-Authored-By: パブロ <111423116+pablo-stelsi@users.noreply.github.com>

--- Assets/_1_Scripts/TrafficSystem/TrafficController.cs
+++ Assets/_1_Scripts/TrafficSystem/TrafficController.cs
@@ -8,6 +8,8 @@ namespace NomadsPlanet
 {
     public class TrafficController : MonoBehaviour
     {
+        private const int SIGN_DURATION = 30;
+        
         private List<TrafficFlow> _trafficFlows;
 
         private void Awake()
@@ -39,7 +41,7 @@ namespace NomadsPlanet
             while (gameObject)
             {
                 // todo: 대충 시간 은근 달라지게끔 해두기
-                int duration = Random.Range(10, 15);
+                int duration = Random.Range(SIGN_DURATION, SIGN_DURATION + 5);
                 for (int i = 0; i < _trafficFlows.Count; i++)
                 {
                     for (int j = 0; j < _trafficFlows.Count; j++)
