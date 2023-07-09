f09ab05bed8de1a8366b5438facf0af75842b4c0
Pablo <pablo@stelsi.org>
2023-07-09 16:33:43 +0900

    차량 이동 시스템 베이스 작업 #1
    Co-Authored-By: pulp-pixel <26397779+pulp-pixel@users.noreply.github.com>

--- Assets/_1_Scripts/TrafficSystem/RoadController.cs
+++ Assets/_1_Scripts/TrafficSystem/RoadController.cs
@@ -3,6 +3,7 @@ using NomadsPlanet.Utils;
 
 namespace NomadsPlanet
 {
+    // Lane Type 없애고.. 타일 별로 계산할 수 있도록
     public class RoadController : MonoBehaviour
     {
         public LaneType LaneType { get; private set; }
