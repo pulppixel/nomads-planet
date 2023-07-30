8587fe9b07f7d960238247aa5be4f8b30581337b
hwankee <devenvy100@gmail.com>
2023-07-30 11:43:21 +0900

    Car In Character

---
+++ Assets/_1_Scripts/Network/JoinServer.cs
@@ -0,0 +1,13 @@
+﻿using Unity.Netcode;
+using UnityEngine;
+
+namespace NomadsPlanet
+{
+    public class JoinServer : MonoBehaviour
+    {
+        public void Join()
+        {
+            NetworkManager.Singleton.StartClient();
+        }
+    }
+}
