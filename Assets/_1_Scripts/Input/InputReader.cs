2202cd73bbbc92362b4b0405f3835cccab82c586
Pablo <devenvy100@gmail.com>
2023-08-01 14:14:31 +0900

    ADD Game Icon

--- Assets/_1_Scripts/Input/InputReader.cs
+++ Assets/_1_Scripts/Input/InputReader.cs
@@ -7,22 +7,21 @@ namespace NomadsPlanet
     [CreateAssetMenu(fileName = "New Input Reader", menuName = "Input/Input Reader", order = 0)]
     public class InputReader : ScriptableObject, Controls.IPlayerActions
     {
-        public event Action<bool> OnPrimaryEvent;
+        // public event Action<bool> OnPrimaryEvent;
 
-        private Controls controls;
+        private Controls _controls;
 
         private void OnEnable()
         {
-            if (controls == null)
+            if (_controls == null)
             {
-                controls = new Controls();
-                controls.Player.SetCallbacks(this);
+                _controls = new Controls();
+                _controls.Player.SetCallbacks(this);
             }
         }
 
         public void OnMove(InputAction.CallbackContext context)
         {
-            
         }
     }
 }
