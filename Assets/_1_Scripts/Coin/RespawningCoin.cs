26af06762db8e9075e9a1866ed2f8bc497b6281f
hwankee <devenvy100@gmail.com>
2023-08-01 23:13:33 +0900

    Update Coin Spawner

--- Assets/_1_Scripts/Coin/RespawningCoin.cs
+++ Assets/_1_Scripts/Coin/RespawningCoin.cs
@@ -1,7 +1,24 @@
-﻿namespace NomadsPlanet
+﻿using System;
+using UnityEngine;
+
+namespace NomadsPlanet
 {
     public class RespawningCoin : Coin
     {
+        public event Action<RespawningCoin> OnCollected;
+
+        private Vector3 previousPosition;
+        
+        private void Update()
+        {
+            if (previousPosition != transform.position)
+            {
+                Show(true);
+            }
+
+            previousPosition = transform.position;
+        }
+
         public override int Collect()
         {
             if (!IsServer)
@@ -17,7 +34,14 @@
 
             alreadyCollected = true;
 
+            OnCollected?.Invoke(this);
+
             return coinValue;
+        }
+
+        public void Reset()
+        {
+            alreadyCollected = false;
         }
     }
 }
