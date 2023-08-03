3acaa6d9c293fa54d330967e9ef55a8ce8387799
Pablo <devenvy100@gmail.com>
2023-08-03 18:37:50 +0900

    Add Characters

---
+++ Assets/_1_Scripts/Player/PlayerSetter.cs
@@ -0,0 +1,12 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace NomadsPlanet
+{
+    public class PlayerSetter : MonoBehaviour
+    {
+        [SerializeField] private List<GameObject> playerPrefabs;
+        
+        
+    }
+}
