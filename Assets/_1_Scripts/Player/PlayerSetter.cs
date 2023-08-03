cfddbc5b0b3f9051ad25d53b15e8a0a07e9c2f1a
Pablo <devenvy100@gmail.com>
2023-08-03 19:16:54 +0900

    Update Animating (Base Work)

--- Assets/_1_Scripts/Player/PlayerSetter.cs
+++ Assets/_1_Scripts/Player/PlayerSetter.cs
@@ -1,12 +1,31 @@
+using System;
 using System.Collections.Generic;
+using NomadsPlanet.Utils;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 namespace NomadsPlanet
 {
     public class PlayerSetter : MonoBehaviour
     {
-        [SerializeField] private List<GameObject> playerPrefabs;
-        
-        
+        public List<GameObject> playerPrefabs;
+
+        public CharacterType setCharacterType;
+
+        private Animator _animator;
+
+        private void Awake()
+        {
+            _animator = GetComponent<Animator>();
+        }
+
+        private void Start()
+        {
+            setCharacterType = (CharacterType)Random.Range(0, 8);
+            var character = Instantiate(playerPrefabs[(int)setCharacterType], this.transform);
+            character.transform.SetSiblingIndex(2);
+            character.gameObject.name = "Player_Model";
+            _animator.Rebind();
+        }
     }
 }
