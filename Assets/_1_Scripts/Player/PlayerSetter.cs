456737b2df40b49eb603f7c5e82b92101421a1f6
Pablo <devenvy100@gmail.com>
2023-08-03 19:55:36 +0900

    For Sync (Character Type)

--- Assets/_1_Scripts/Player/PlayerSetter.cs
+++ Assets/_1_Scripts/Player/PlayerSetter.cs
@@ -1,12 +1,13 @@
 using System;
 using System.Collections.Generic;
 using NomadsPlanet.Utils;
+using Unity.Netcode;
 using UnityEngine;
 using Random = UnityEngine.Random;
 
 namespace NomadsPlanet
 {
-    public class PlayerSetter : MonoBehaviour
+    public class PlayerSetter : NetworkBehaviour
     {
         public List<GameObject> playerPrefabs;
 
@@ -22,6 +23,10 @@ namespace NomadsPlanet
         private void Start()
         {
             setCharacterType = (CharacterType)Random.Range(0, 8);
+        }
+
+        public override void OnNetworkSpawn()
+        {
             var character = Instantiate(playerPrefabs[(int)setCharacterType], this.transform);
             character.transform.SetSiblingIndex(2);
             character.gameObject.name = "Player_Model";
