43bb0be04892a8d5447b891696c2f3a9b24dcc8a
hwankee <devenvy100@gmail.com>
2023-08-13 13:13:48 +0900

    어느 씬에 있던지 첫 씬에서 시작할 수 있도록 하기
    이거 namespace 안붙여야 정상 작동함
    
    Co-Authored-By: パブロ <111423116+pablo-stelsi@users.noreply.github.com>

---
+++ Assets/_Editor/StartupSceneLoader.cs
@@ -0,0 +1,28 @@
+﻿using UnityEditor;
+using UnityEditor.SceneManagement;
+using UnityEngine.SceneManagement;
+
+[InitializeOnLoad]
+public static class StartupSceneLoader
+{
+    static StartupSceneLoader()
+    {
+        EditorApplication.playModeStateChanged += LoadStartupScene;
+    }
+
+    private static void LoadStartupScene(PlayModeStateChange state)
+    {
+        if (state == PlayModeStateChange.ExitingEditMode)
+        {
+            EditorSceneManager.SaveCurrentModifiedScenesIfUserWantsTo();
+        }
+
+        if (state == PlayModeStateChange.EnteredPlayMode)
+        {
+            if (SceneManager.GetActiveScene().buildIndex != 0)
+            {
+                SceneManager.LoadScene(0);
+            }
+        }
+    }
+}
