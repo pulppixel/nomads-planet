712f701166071aa2320d74b0fc314d1ef90bca81
hwankee <devenvy100@gmail.com>
2023-08-27 22:55:27 +0900

    Addressable 도입

---
+++ Assets/_1_Scripts/Network/AddressableManager.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NomadsPlanet.Utils;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.AddressableAssets;
+using UnityEngine.ResourceManagement.AsyncOperations;
+
+namespace NomadsPlanet
+{
+    public class AddressableManager : MonoBehaviour
+    {
+        [SerializeField] private Slider downloadProgressSlider;
+        [SerializeField] private TMP_Text downloadText;
+
+        private void Start()
+        {
+            downloadText.text = string.Empty;
+        }
+
+        public IEnumerator DownloadAllAssets()
+        {
+            var checkHandle = Addressables.CheckForCatalogUpdates(false);
+            yield return checkHandle;
+
+            if (checkHandle.Status == AsyncOperationStatus.Succeeded)
+            {
+                List<string> catalogs = checkHandle.Result;
+                if (catalogs is { Count: > 0 })
+                {
+                    var dlHandle = Addressables.DownloadDependenciesAsync(catalogs, true);
+
+                    // 여기서 진행 상황을 확인
+                    while (!dlHandle.IsDone)
+                    {
+                        downloadProgressSlider.value = dlHandle.PercentComplete;
+                        downloadText.text = $"Loading... {dlHandle.PercentComplete}";
+                        yield return null;
+                    }
+
+                    if (dlHandle.Status == AsyncOperationStatus.Failed)
+                    {
+                        CustomFunc.ConsoleLog("Failed to download dependencies.");
+                    }
+                }
+            }
+
+            Addressables.Release(checkHandle);
+        }
+
+    }
+}
