e2321c6b1ec2a230e85d7ec93e0c39d6d5f446e0
hwankee <devenvy100@gmail.com>
2023-09-09 01:07:23 +0900

    Menu UI Work #1
    꽤... 오래걸릴지도..

--- Assets/_1_Scripts/Core/Interactions/MenuInteraction.cs
+++ Assets/_1_Scripts/Core/Interactions/MenuInteraction.cs
@@ -1,9 +1,11 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 
 namespace NomadsPlanet
 {
     public class MenuInteraction : MonoBehaviour
     {
-        
+        [SerializeField] private Image chooseMenu;
+        // [SerializeField] private Image 
     }
 }
