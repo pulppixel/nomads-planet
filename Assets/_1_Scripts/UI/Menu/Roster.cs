18c72037c37ae555ca8684f814073aa85dabd781
hwankee <devenvy100@gmail.com>
2023-09-10 16:32:14 +0900

    Update Roster

---
+++ Assets/_1_Scripts/UI/Menu/Roster.cs
@@ -0,0 +1,20 @@
+﻿using TMPro;
+using UnityEngine;
+using VivoxUnity;
+
+namespace NomadsPlanet
+{
+    public class Roster : MonoBehaviour
+    {
+        [SerializeField] private TMP_Text nameText;
+
+        public IParticipant Participant { get; private set; }
+        
+        public void SetupRosterItem(IParticipant participant)
+        {
+            Participant = participant;
+            nameText.text = participant.Account.DisplayName;
+        }
+        
+    }
+}
