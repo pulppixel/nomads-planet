95607f7d2e96ef11fbfeb063c1ea77e4d02c13fd
hwankee <devenvy100@gmail.com>
2023-09-18 02:12:58 +0900

    Update Intro Timeline (Almost Done!!)

---
+++ Assets/_1_Scripts/Sound/SfxPlayer.cs
@@ -0,0 +1,23 @@
+﻿using NomadsPlanet.Utils;
+using UnityEngine;
+
+namespace NomadsPlanet
+{
+    [RequireComponent(typeof(AudioSource))]
+    public class SfxPlayer : MonoBehaviour
+    {
+        private AudioSource _audioSource;
+
+        [SerializeField] private AudioClip[] audioClips;
+
+        private void Awake()
+        {
+            _audioSource = GetComponent<AudioSource>();
+        }
+
+        public void PlaySfx(int idx)
+        {
+            _audioSource.PlayOneShot(audioClips[idx]);
+        }
+    }
+}
